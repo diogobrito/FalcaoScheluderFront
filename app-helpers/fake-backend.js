b6a8f09a4faf832b60aa056a71dd4f2f46f6a5fe
Diogo Brito <diogobrito2000@yahoo.com.br>
2018-04-14 13:49:33 -0300

    moc logado

--- app-helpers/fake-backend.js
+++ app-helpers/fake-backend.js
@@ -5,6 +5,8 @@
         .module('app')
         .run(setupFakeBackend);
 
+
+        
     // setup fake backend for backend-less development
     function setupFakeBackend($httpBackend) {
         var testUser = { username: 'test', password: 'test', firstName: 'Test', lastName: 'User' };
@@ -13,13 +15,36 @@
         $httpBackend.whenPOST('/api/authenticate').respond(function (method, url, data) {
             // get parameters from post request
             var params = angular.fromJson(data);
+            console.log("logado");
 
+            
             // check user credentials and return fake jwt token if valid
             if (params.username === testUser.username && params.password === testUser.password) {
                 return [200, { token: 'fake-jwt-token' }, {}];
             } else {
                 return [200, {}, {}];
             }
+        });
+        
+        $httpBackend.whenGET('/api/profissionais').respond(function (method, url, data) {
+            return [200, { profissionais: [{ id: 1, nome: "Denis" }, { id: 2, nome: "Marcelo" }]}, {}];
+        });
+        
+        $httpBackend.whenGET('/api/profissional/:id/agenda').respond(function (method, url, data, headers, params) {
+            return [200, {  }, {}];
+        });
+
+        $httpBackend.whenPOST('/api/profissional/:id/agenda').respond(function (method, url, data, headers, params) {
+            // get parameters from post request
+            var params = angular.fromJson(data);
+
+            /*{
+                "professionalId": 1,
+                "dataInicio": "2010-06-09T15:20:00Z",
+                "usuarioId": 1
+            }*/
+            
+            return [200, {  }, {}];
         });
 
         // pass through any urls not handled above so static files are served correctly
